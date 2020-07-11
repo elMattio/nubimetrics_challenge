1d660d83e560a2e9fb747ee668367e996f75e313
Mattio <matiasb.murga@gmail.com>
2020-07-11 15:24:26 -0300

    Modelo de ventana modal funcionando

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,37 +1,20 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import { connect } from "react-redux";
 import { closeModal } from "../actions/index";
+import { Modal } from "react-bootstrap";
 
-function Modal({isModalOpen, closeModal}) {
-    if(isModalOpen) {
-        return (
-             ReactDOM.createPortal(
-                
-                <div className="modal-dialog modal-dialog-centered" role="document">
-                  <div className="modal-content">
-                    <div className="modal-header">
-                      <h5 className="modal-title" id="exampleModalCenterTitle">Modal title</h5>
-                      <button onClick={closeModal} type="button" className="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                      </button>
-                    </div>
-                    <div className="modal-body">
-                      ...
-                    </div>
-                    <div className="modal-footer">
-                      <button onClick={closeModal} type="button" className="btn btn-outline-success my-2 my-sm-0" data-dismiss="modal">Cerrar</button>
-                    </div>
-                  </div>
-                </div>
-                
-              ,
-           document.getElementById("modal")
-            )
+function ModalW({isModalOpen, closeModal}) {
+        return (     
+          <div >
+            <Modal show={isModalOpen} onHide={closeModal}>
+              <Modal.Header closeButton>Este es el modal</Modal.Header>
+              <Modal.Body>Este es el cuerpito de mi modal</Modal.Body>
+              <Modal.Footer>
+                <button onClick={closeModal} className="btn btn-outline-success my-2 my-sm-0">Cerrar</button>
+              </Modal.Footer>
+            </Modal>
+          </div>  
         )
-    } else {
-        return null
-    }
 };
 
 function mapStateToProps(state) {
@@ -40,4 +23,4 @@ function mapStateToProps(state) {
     }
 }
 
-export default connect(mapStateToProps, { closeModal} )(Modal)
+export default connect(mapStateToProps, { closeModal} )(ModalW)
